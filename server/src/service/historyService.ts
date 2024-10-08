558523582515bab2fae39ecf207e14c0323781a5
josh-hensley <me@joshhensley.com>
2024-10-08 17:16:13 -0500

    implemented some todos on server.ts, started work on classes in historyService

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,15 +1,40 @@
-// TODO: Define a City class with name and id properties
+import fs from "fs";
+
+class City {
+  name: string;
+  id: number;
+  constructor (name: string, id: number){
+    this.name = name;
+    this.id = id;
+  } 
+}
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read(): Promise<string> {
+    const response = await fs.readFile(
+      '../../db/searchHistory.json',
+      'utf-8', 
+      (err) => { err ? console.log(err) : console.log('Search History Read Successfully!') });
+    return response;
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    let currentCities = this.getCities();
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities(): Promise<City[]> {
+    let cities: City[] = [];
+    const data = await this.read();
+    data.forEach(item=>{
+      const city = new City(item.name, item.id);
+      cities.push(city); 
+    });
+    return cities;
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
